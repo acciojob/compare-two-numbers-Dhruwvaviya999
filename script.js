bf1c231193581d422925716730412d9f7639fcc5
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-22 01:48:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let firstNumber = prompt("Enter First Number: ");
+let secondNumber = prompt("Enter Second Number: ");
+
+if(firstNumber == secondNumber){
+	alert(true);
+} else {
+	alert(false);
+}
